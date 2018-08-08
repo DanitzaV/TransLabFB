6a4fda50eff0e483f70c86f118f5520653223bfc
viviana <dmpviviana@gmail.com>
2018-08-07 22:41:45 -0400

    modificacion nabvar

--- src/js/data.js
+++ src/js/data.js
@@ -1,53 +1,54 @@
 $(document).ready(function(){
     $('.sidenav').sidenav();
   });
-  function registro() {
-    const emailValue = email.value;
-    const passwordValue = password.value;
-    if (passwordValue.length <=8 && passwordValue.length  >3){
+
+function registro() {
+  const emailValue = email.value;
+  const passwordValue = password.value;
+  if (passwordValue.length <= 8 && passwordValue.length > 3) {
     firebase.auth().createUserWithEmailAndPassword(emailValue, passwordValue)
-    .then((response) => {
+      .then((response) => {
         location = '../../dist/index.html'
         console.log('usuario registrado');
-     })
-    .catch((error) => {
-      console.log("Error de firebase > Código > "+error.code);
-      console.log("Error de firebase > Mensaje > "+error.message);
-     });
+      })
+      .catch((error) => {
+        console.log("Error de firebase > Código > " + error.code);
+        console.log("Error de firebase > Mensaje > " + error.message);
+      });
   }
 }
 function login() {
-    const emailValue = email.value;
-      const passwordValue = password.value
-        firebase.auth().signInWithEmailAndPassword(emailValue, passwordValue)
-        .then(()=>{
-            
-          location = '../../dist/html/perfil.html';
-          console.log("Usuario con login exitoso");
-        })
-      .catch((error)=>{
-        console.log("Error de firebase > "+error.code);
-        console.log("Error de firebase, mensaje > "+error.message);
-      });
-      
-     
+  const emailValue = email.value;
+  const passwordValue = password.value
+  firebase.auth().signInWithEmailAndPassword(emailValue, passwordValue)
+    .then(() => {
+
+      location = '../../dist/html/perfil.html';
+      console.log("Usuario con login exitoso");
+    })
+    .catch((error) => {
+      console.log("Error de firebase > " + error.code);
+      console.log("Error de firebase, mensaje > " + error.message);
+    });
+
+
 }
 function cerrarSesion() {
   firebase.auth().signOut()
- .then(()=>{
- location = '../../dist/index.html';
-   })
- .catch();
+    .then(() => {
+      location = '../../dist/index.html';
+    })
+    .catch();
 }
 
-function agregarTarjeta(){
+function agregarTarjeta() {
   const currentUser = firebase.auth().currentUser;
   const numeroBip = icon_telephone.value;
   const tarjetas = firebase.database().ref().child('bips').push().key;
 
-firebase.database().ref(`bips/${tarjetas}`).set({
-    creator : currentUser.uid,
-   numBip: numeroBip
-});
-console.log(currentUser);
+  firebase.database().ref(`bips/${tarjetas}`).set({
+    creator: currentUser.uid,
+    numBip: numeroBip
+  });
+  console.log(currentUser);
 }
